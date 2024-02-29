ab8b5e6744cec710ab8df24019354239006c8e81
Aditya Ghosh <adi27.ghosh@gmail.com>
2024-02-29 20:07:22 +0530

    prototype of the game is completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,35 @@
+import { useState } from "react";
+
 export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value="1"/>
-        <Square value="2" />
-        <Square value="3"/>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="4"/>
-        <Square value="5"/>
-        <Square value="6"/>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="7"/>
-        <Square value="8"/>
-        <Square value="9"/>
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
 }
 
-function Square({ value } : { value : string}){
+function Square(){
+  const [value, setValue] = useState<null | string>(null);
+
+  function handleClick(){
+    setValue('X');
+  }
+
   return (
-    <button className="square">{value}</button>
+    <button className="square" onClick={handleClick}>{value}</button>
   )
 }
