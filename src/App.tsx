ba41657683dcdf62d602432afa9d7d3fd4bb63f8
Aditya Ghosh <adi27.ghosh@gmail.com>
2024-03-01 22:54:17 +0530

    added interface to the square component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
 import { useState } from "react";
 
 export default function Board() {
+  const [square, setSquare] = useState< null[] | string[] >(Array(9).fill(null));
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={square[0]} onSquareClick={handleClick}/>
+        <Square value={square[1]}/>
+        <Square value={square[2]}/>
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={square[3]}/>
+        <Square value={square[4]}/>
+        <Square value={square[5]}/>
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={square[6]}/>
+        <Square value={square[7]}/>
+        <Square value={square[8]}/>
       </div>
     </>
   );
 }
 
-function Square(){
-  const [value, setValue] = useState<null | string>(null);
+interface MyButtonProps {
+  value : null | string,
+  onSquareClick : null | string
+}
 
-  function handleClick(){
-    setValue('X');
-  }
+function Square({ value, onSquareClick } : MyButtonProps){
 
   return (
-    <button className="square" onClick={handleClick}>{value}</button>
+    <button className="square" onClick={onSquareClick}>{value}</button>
   )
 }
